8eb28aa39e65a5fa9f9038827b62bfd591e0f43a
silva3265 <silva3265@gmail.com>
2024-09-16 17:12:26 -0300

    Vetores e Arrays - Adicionar elemento no final do vetor

--- src/estruturadados/vetor/Vetor.java
+++ src/estruturadados/vetor/Vetor.java
@@ -7,7 +7,33 @@ public class Vetor {
 
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade]; // A capacidade estamos passando para o vetor
-		this.tamanho = 0;
+		this.tamanho = 0; // Variavel de Controle
 	}
 
+//	public void adiciona(String elemento) {
+//		for (int i = 0; i < this.elementos.length; i++) { // Iterando todo o vetor
+//			if (this.elementos[i] == null) { // verificando se o elemento na posição I esta null
+//				this.elementos[i] = elemento; // se tiver, essa posição null recebe elemento
+//				break; // o break vai parar a se ser executado na primeira posição que ele encontrar nula
+//			}
+//		}
+//	}
+
+//	public void adiciona(String elemento) throws Exception {
+//		if (this.tamanho < this.elementos.length) { // Verificando se o tamanaho for Menor do que a capacidade do Vetor de Elementos
+//			this.elementos[this.tamanho] = elemento; // adicionando o elemento
+//			this.tamanho++; // incrementando 1
+//		} else {
+//			throw new Exception("Vetor já está cheio, não é possível adicionar mais elementos");
+//		}
+//	}
+	
+	public boolean adiciona(String elemento) {
+		if (this.tamanho < this.elementos.length){
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		} 
+		return false;
+	}
 }
---
+++ src/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,16 @@
+package estruturadados.vetor.teste;
+
+import estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(2);
+
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+	}
+
+}
