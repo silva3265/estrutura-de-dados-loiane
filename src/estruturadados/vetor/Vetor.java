335e0314b683ebcbf958394c30171a2b066865f3
silva3265 <silva3265@gmail.com>
2024-09-19 15:41:59 -0300

    Vetores e Arrays- tamanho e imprimir elementos

--- src/estruturadados/vetor/Vetor.java
+++ src/estruturadados/vetor/Vetor.java
@@ -27,13 +27,38 @@ public class Vetor {
 //			throw new Exception("Vetor já está cheio, não é possível adicionar mais elementos");
 //		}
 //	}
-	
+
 	public boolean adiciona(String elemento) {
-		if (this.tamanho < this.elementos.length){
+		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
 			this.tamanho++;
 			return true;
-		} 
+		}
 		return false;
 	}
+
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+
+		for (int i = 0; i < this.tamanho - 1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+
+		if (this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho - 1]);
+		}
+
+		s.append("]");
+
+		return s.toString();
+	}
+
 }
